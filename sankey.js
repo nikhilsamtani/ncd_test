2bada5d1b111e5a19c996155359408bb8da109d2
Nikhil Samtani <nikhil29@uw.edu>
2018-05-07 20:51:44 -0700

    checkboxes

--- sankey.js
+++ sankey.js
@@ -127,7 +127,7 @@ d3.sankey = function() {
 
     //
     moveSinksRight(x);
-    scaleNodeBreadths((width - 100 - nodeWidth) / (x - 1));
+    scaleNodeBreadths((width - 150 - nodeWidth) / (x - 1));
   }
 
   function moveSourcesRight() {
